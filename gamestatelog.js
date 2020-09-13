8974eb84bce2158f0828bdc05875bbc301fb91e0
Oznogon <garrett+ozgh@oznogon.com>
2020-09-12 19:58:10 -0700

    Deep linting of LogData class

--- gamestatelog.js
+++ gamestatelog.js
@@ -1,71 +1,82 @@
 /* eslint-env jquery */
-/* eslint semi: "error" */
+/* eslint semi: "error", indent: ["error", 2] */
+/* eslint no-magic-numbers: ["error", { "ignoreArrayIndexes": true }] */
+/* eslint no-magic-numbers: ["error", { "ignore": [1] }] */
+/* eslint padded-blocks: ["error", "never"] */
+/* eslint function-call-argument-newline: ["error", "never"] */
+/* eslint max-len: ["warn", { "code": 120 }] */
+/* eslint-disable max-classes-per-file, no-console, max-statements */
 
-class LogData
-{
-  constructor(text)
-  {
+// Consume and organize EmptyEpsilon game state log data.
+class LogData {
+  constructor (text) {
+    // Delineate text by CRLF line endings.
+    const lines = text.match(/^.*(?<id>[\n\r]+|$)/ugm);
+
     this.entries = [];
 
-    // Delineate text by CRLF line endings.
-    var lines = text.match(/^.*([\n\r]+|$)/gm);
-
-    // Parse each line if it's valid.
-    for (var index = 0; index < lines.length; index++)
-    {
-      try
-      {
-        if (lines[index].trim() != "") {
-          this.entries.push(JSON.parse(lines[index]));
+    // Parse each line if it's valid, and throw an error if it's not.
+    for (let lineIndex = 0; lineIndex < lines.length; lineIndex += 1) {
+      try {
+        if (lines[lineIndex].trim() !== "") {
+          this.entries.push(JSON.parse(lines[lineIndex]));
         }
       } catch (err) {
         console.debug("Read json line error: ", err);
       }
     }
 
-    console.debug("Loaded: " + this.entries.length + " log entries");
+    console.debug(`Loaded: ${this.entries.length} log entries`);
   }
 
-  getMaxTime()
-  {
-    return this.entries[this.entries.length-1].time;
+  // Get the total scenario runtime from the last entry's timestamp.
+  getMaxTime () {
+    return this.entries[this.entries.length - 1].time;
   }
 
-  getEntriesAtTime(time)
-  {
-    var last_objects = {};
-    var static_objects = {};
+  // Get log entries for a given point in the scenario timeline.
+  getEntriesAtTime (time) {
+    let lastObjects = {};
+    const staticObjects = {};
 
-    for (var index = 0; index < this.entries.length; index++)
-    {
-      var entry = this.entries[index];
+    for (let entryIndex = 0; entryIndex < this.entries.length; entryIndex += 1) {
+      const entry = this.entries[entryIndex];
 
+      /*
+       * Work from beginning to end, and stop if the entry timestamp is later than the given time.
+       * We collect all entries up to the given time, because static objects are only guaranteed to
+       * be listed when they're added and might be modified or deleted between the scenario start
+       * and the given time, so we need to compare any added or deleted objects to the previous
+       * cumulative state.
+       */
       if (entry.time > time) {
         break;
       }
 
-      last_objects = entry.objects;
+      // Collect all objects into the state at the point in time being added to the data.
+      lastObjects = entry.objects;
 
-      for (var new_static_index = 0; new_static_index < entry.new_static.length; new_static_index++)
-      {
-        var obj = entry.new_static[new_static_index];
-        static_objects[obj.id] = obj;
+      // Add each static object in the entry to a running list of static objects.
+      for (let newStaticIndex = 0; newStaticIndex < entry.new_static.length; newStaticIndex += 1) {
+        const object = entry.new_static[newStaticIndex];
+        staticObjects[object.id] = object;
       }
 
-      for (var del_static_index = 0; del_static_index < entry.del_static.length; del_static_index++)
-      {
-        var obj_id = entry.del_static[del_static_index];
-        delete static_objects[obj_id];
+      // Delete each static object flagged in the entry from the list of static objects.
+      for (let delStaticIndex = 0; delStaticIndex < entry.del_static.length; delStaticIndex += 1) {
+        const objectId = entry.del_static[delStaticIndex];
+        delete staticObjects[objectId];
       }
     }
 
-    for(var last_objects_index = 0; last_objects_index < last_objects.length; last_objects_index++)
-    {
-      var last_object = last_objects[last_objects_index];
-      static_objects[last_object.id] = last_object;
+    // Update the state of all existing objects using the currently examined entry.
+    for (let lastObjectsIndex = 0; lastObjectsIndex < lastObjects.length; lastObjectsIndex += 1) {
+      const lastObject = lastObjects[lastObjectsIndex];
+      staticObjects[lastObject.id] = lastObject;
     }
 
-    return static_objects;
+    // Return the updated list of static objects.
+    return staticObjects;
   }
 }
 
